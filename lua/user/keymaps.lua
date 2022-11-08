9b2575d86fa4e8f3b8d27aef719fd0c0140b1069
legendary2017 <827788294@qq.com>
2022-11-08 04:03:59 -0500

    update keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -19,3 +19,10 @@ map('v', '>', '>gv', noremap)
 
 -- Go back to normal mode in terminal
 map('t', '<Esc>', '<C-\\><C-n>', noremap)
+
+-- Terminal --
+-- Better terminal navigation
+map("t", "<C-w>h", "<C-\\><C-n><C-w>h")
+map("t", "<C-w>j", "<C-\\><C-n><C-w>j")
+map("t", "<C-w>k", "<C-\\><C-n><C-w>k")
+map("t", "<C-w>l", "<C-\\><C-n><C-w>l")
