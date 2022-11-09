c5d45466d68273c85ad55718c82593078b850beb
legendary2017 <827788294@qq.com>
2022-11-09 01:49:52 -0500

    update config

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,22 +1,31 @@
-local status_ok, null_ls = pcall(require, "null-ls")
+local status_ok, null_ls = pcall(require, 'null-ls')
 if not status_ok then
     return
 end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#formatting
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
--- local actions = null_ls.builtins.code_actions
+local actions = null_ls.builtins.code_actions
 
 null_ls.setup {
     debug = false,
     sources = {
+        -- Git
+        actions.gitsigns,
+
         -- Python
         -- Pylint is a Python static code analysis tool.
         diagnostics.pylint,
+        -- flake8 is a python tool that glues together pycodestyle, pyflakes, mccabe, and third-party plugins to check the style and quality of Python code.
+        diagnostics.flake8,
+        -- Vulture finds unused code in Python programs.
+        diagnostics.vulture,
         -- Python utility / library to sort imports alphabetically and automatically separate them into sections and by type.
         formatting.isort,
         -- The uncompromising Python code formatter.
         formatting.black,
+        -- Tool for automatically reordering python imports. Similar to isort but uses static analysis more.
+        formatting.reorder_python_imports,
 
         -- Golang
         -- A Go linter aggregator.
