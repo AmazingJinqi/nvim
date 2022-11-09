1c26f19825f1c8da93573199306277706c2e9798
legendary2017 <827788294@qq.com>
2022-11-09 02:49:50 -0500

    update config

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -83,25 +83,6 @@ local function lsp_keymaps(bufnr)
     map('n', '<leader>ln', vim.lsp.buf.rename, bufopts('Rename'))
     map('n', '<leader>lc', vim.lsp.buf.code_action, bufopts('Code actions'))
 
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts('Go to declaration'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts('Go to definition'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts('LSP hover'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>",
-    --     opts('Go to implementation'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts('Show signature'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts('Go to references'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>',
-    --     opts('Go to previous diagnostics'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gl",
-    --     '<cmd>lua vim.diagnostic.open_float({ border = "rounded", scope = "line" })<CR>',
-    --     opts('Show diagnostics in the current line'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
-    --     opts('Go to next diagnostics'))
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>",
-    --     opts('Add buffer diagnostics to the location list'))
-
     vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format { async = true }' ]]
 
     map('n', '<leader>/', '<cmd>Format<cr>', bufopts('Format'))
@@ -114,6 +95,7 @@ M.on_attach = function(client, bufnr)
     -- if client.name == "tsserver" then
     --     client.server_capabilities.documentFormattingProvider = false
     -- end
+
     lsp_keymaps(bufnr)
     lsp_highlight_document(client)
 end
